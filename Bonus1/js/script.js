13570cef0b5fc94a9911280aadadd49c180db6ce
FontaMatte <matteo.fontanel@gmail.com>
2022-12-22 11:01:14 +0100

    Code Cleaning

--- Bonus1/js/script.js
+++ Bonus1/js/script.js
@@ -6,52 +6,44 @@ for (let i = 1 ; i <= 100 ; i++) {
 
     // verifico se i e' un multiplo di sia di 3 che di 5
     if ((i % 5 == 0) && (i % 3 == 0)) {
-        // const element = `<div class="box">FizzBuzz</div>`;
-        // boxContainer.innerHTML += element;
 
-        // Creo un container nel DOM utilizzando append() dove inserire la stringa o il numero.
+        // Creo un div nel DOM , ci inserisco la stringa o il numero ed infine lo "aapendo" al container padre
         const newDiv = document.createElement('div');
         newDiv.innerHTML = 'FizzBuzz';
-        newDiv.className = 'box-fizzbuzz';
+        // newDiv.className = 'box-fizzbuzz';
         boxContainer.append(newDiv);
         console.log(newDiv);    
     }
 
     // verifico se i e' un multiplo di 3
     else if (i % 3 == 0) {
-        // boxContainer.innerHTML += element;const element = `<div class="box">Fizz</div>`;
-        // boxContainer.innerHTML += element;
-
-        // Creo un container nel DOM utilizzando append() dove inserire la stringa o il numero.
+        
+        // Creo un div nel DOM , ci inserisco la stringa o il numero ed infine lo "aapendo" al container padre
         const newDiv = document.createElement('div');
         newDiv.innerHTML = 'Fizz';
-        newDiv.className = 'box-fizz';
+        // newDiv.className = 'box-fizz';
         boxContainer.append(newDiv);
         console.log(newDiv);  
     }
 
     // verifico se i e' un multiplo di 5
     else if (i % 5 == 0) {
-        // boxContainer.innerHTML += element;const element = `<div class="box">Buzz</div>`;
-        // boxContainer.innerHTML += element;
 
-        // Creo un container nel DOM utilizzando append() dove inserire la stringa o il numero.
+        // Creo un div nel DOM , ci inserisco la stringa o il numero ed infine lo "aapendo" al container padre
         const newDiv = document.createElement('div');
         newDiv.innerHTML = 'Buzz';
-        newDiv.className = 'box-buzz';
+        // newDiv.className = 'box-buzz';
         boxContainer.append(newDiv);
         console.log(newDiv);  
     }
 
     // Se non si verifica nessuna delle precedenti condizioni stampo "i"
     else {
-        // const element = `<div class="box">${i}</div>`;
-        // boxContainer.innerHTML += element; 
 
-        // Creo un container nel DOM utilizzando append() dove inserire la stringa o il numero.
+        // Creo un div nel DOM , ci inserisco la stringa o il numero ed infine lo "aapendo" al container padre
         const newDiv = document.createElement('div');
         newDiv.innerHTML = `${i}`;
-        newDiv.className = 'box';
+        // newDiv.className = 'box';
         boxContainer.append(newDiv);
         console.log(newDiv);  
     }   
